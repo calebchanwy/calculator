f8e046b7e8f670df48f8b3992ceeed779edcddfa
calebchanwy <calebchanwy@gmail.com>
2025-01-06 16:51:43 +0000

    Add sum logic, controller and unit test

---
+++ src/test/java/com/calebchanwy/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.calebchanwy.calculator;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2,3));
+    }
+}
